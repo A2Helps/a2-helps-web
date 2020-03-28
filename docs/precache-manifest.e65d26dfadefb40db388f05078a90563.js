48ee8f5c3595316934f59238bd12305ab26b9299
Jim Hall <jim.hall.dev@gmail.com>
2020-03-28 13:52:40 -0400

    publish

---
+++ docs/precache-manifest.e65d26dfadefb40db388f05078a90563.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9cb1ecdd9a85a3f1412e6558bc05a355",
+    "url": "/index.html"
+  },
+  {
+    "revision": "32409082e25393db11dc",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "673cab750c4b1bf5e261",
+    "url": "/static/js/2.da004146.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.da004146.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "32409082e25393db11dc",
+    "url": "/static/js/main.2788eb41.chunk.js"
+  },
+  {
+    "revision": "33ff5253cefcdaca9c9a",
+    "url": "/static/js/runtime-main.547fe968.js"
+  }
+]);
