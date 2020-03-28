4d23dd361f74fad80f66180c320f3abd7e2fae50
Patrick McCarren <patrick@cumulative.io>
2020-03-28 12:08:26 -0400

    feat: install api client and base Model

--- src/index.js
+++ src/index.js
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import axios from 'axios';
+import { Model } from 'vue-api-query';
+
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
@@ -15,3 +19,6 @@ ReactDOM.render(
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: https://bit.ly/CRA-PWA
 serviceWorker.unregister();
+
+// inject global axios instance as http client to Model
+Model.$http = axios
