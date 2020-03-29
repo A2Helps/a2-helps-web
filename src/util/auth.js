1173a306cee238b0bc51454c141753cc7c77b600
Dylan Goings <dylan@dgoings.com>
2020-03-29 11:42:45 -0400

    convert tabs to spaces

--- src/util/auth.js
+++ src/util/auth.js
@@ -17,49 +17,49 @@ import 'firebase/auth';
 // };
 
 var firebaseConfig = {
-	apiKey: "AIzaSyDSRVTsV7WGERtOgMtixEVdSjczs0ORMac",
-	authDomain: "a2cares-local.firebaseapp.com",
-	databaseURL: "https://a2cares-local.firebaseio.com",
-	projectId: "a2cares-local",
-	storageBucket: "a2cares-local.appspot.com",
-	messagingSenderId: "581955185890",
-	appId: "1:581955185890:web:8a595e40b1481ac02742bb",
-	measurementId: "G-8N0PBQJZ2X"
+  apiKey: "AIzaSyDSRVTsV7WGERtOgMtixEVdSjczs0ORMac",
+  authDomain: "a2cares-local.firebaseapp.com",
+  databaseURL: "https://a2cares-local.firebaseio.com",
+  projectId: "a2cares-local",
+  storageBucket: "a2cares-local.appspot.com",
+  messagingSenderId: "581955185890",
+  appId: "1:581955185890:web:8a595e40b1481ac02742bb",
+  measurementId: "G-8N0PBQJZ2X"
 };
 
 firebase.initializeApp(firebaseConfig);
 export const auth = (email, password) => {
-	firebase.auth().onAuthStateChanged(function(user) {
-		if (user) {
-			// User is signed in.
-			// var displayName = user.displayName;
-			var userEmail = user.email;
-			// var emailVerified = user.emailVerified;
-			// var photoURL = user.photoURL;
-			// var isAnonymous = user.isAnonymous;
-			var uid = user.uid;
-			// var providerData = user.providerData;
+  firebase.auth().onAuthStateChanged(function(user) {
+    if (user) {
+      // User is signed in.
+      // var displayName = user.displayName;
+      var userEmail = user.email;
+      // var emailVerified = user.emailVerified;
+      // var photoURL = user.photoURL;
+      // var isAnonymous = user.isAnonymous;
+      var uid = user.uid;
+      // var providerData = user.providerData;
 
-			console.log(userEmail, uid);
+      console.log(userEmail, uid);
 
-			user.getIdToken().then(token => {
-				console.log(token);
-			})
+      user.getIdToken().then(token => {
+        console.log(token);
+      })
 
-			// ...
-		} else {
-			console.log('signed out');
-		}
-	});
+      // ...
+    } else {
+      console.log('signed out');
+    }
+  });
 
 
-	firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
-		// Handle Errors here.
-		var errorCode = error.code;
-		var errorMessage = error.message;
-		// ...
+  firebase.auth().signInWithEmailAndPassword(email, password).catch(function(error) {
+    // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // ...
 
-		console.log(errorCode, errorMessage);
-	});
+    console.log(errorCode, errorMessage);
+  });
 }
 
