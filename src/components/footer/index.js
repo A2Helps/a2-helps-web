9f612ce7f7c35ab2163f3144dc04d3f017996e5d
hanacoon <hanacoon@Hanas-MacBook-Pro-2.local>
2020-04-03 12:18:07 -0400

    Fixing footer issues & updating with correct social links.

--- src/components/footer/index.js
+++ src/components/footer/index.js
@@ -10,21 +10,31 @@ import {
 const useStyles = makeStyles(theme => ({
   root: {
     textAlign: 'center',
-    [theme.breakpoints.up('sm')]: {
+    [theme.breakpoints.up('md')]: {
       textAlign: 'left',
     },
     backgroundColor: '#ffffff',
     padding: 40,
     marginTop: 40,
   },
+  container: {
+    paddingLeft: '0px',
+    paddingRight: '0px',
+  },
   img: {
-    width: '25px',
+    width: '30px',
     marginTop: 20,
-    [theme.breakpoints.up('sm')]: {
-      marginTop: 0,
-    },
+    
     [theme.breakpoints.up('md')]: {
       width:'35px',
+      marginTop: 0,
+    },
+  },
+  socialLinks: {
+    maxWidth: '250px',
+    margin: '0 auto',
+    [theme.breakpoints.up('md')]: {
+      margin: '0',
     },
   },
 }));
@@ -34,14 +44,15 @@ export default function Footer() {
 
   return (
     <div className={classes.root}>
-      <Container>
+      <Container className={classes.container}>
 
         <Grid
           container
-          alignItems="center"
+          alignItems='center'
+          justify='space-between'
         >
 
-          <Grid item xs={12} sm={9}>
+          <Grid item xs={12} md={9}>
             <Typography variant="body1" color="inherit">
               Made with <strong style={{color: "#F85A3E"}}>♥</strong> by volunteers in Ann Arbor, MI.
             </Typography>
@@ -51,28 +62,34 @@ export default function Footer() {
             item 
             container
             xs={12}
-            sm={3}
-            style={{maxWidth: '200px', margin: '0 auto'}}
+            md={3}
+            justify='space-between'
+            className={classes.socialLinks}
           >
-            <Grid item xs={3}>
-              <Link to="/">
+            <Grid item xs={2}>
+              <a href="mailto:help@a2helps.com" target="_top">
                 <img className={classes.img} src="email-icon.svg" alt="Email Icon" />
-              </Link>
+              </a>
             </Grid>
-            <Grid item xs={3}>
-              <Link to="/">
-                <img className={classes.img} src="facebook-icon.svg" alt="Facebook Icon" />
-              </Link>
-            </Grid>
-            <Grid item xs={3}>
-              <Link to="/">
+            <Grid item xs={2}>
+              <a target="_blank" href="https://www.instagram.com/a2helps/">
                 <img className={classes.img} src="instagram-icon.svg" alt="Instagram Icon" />
-              </Link>
+              </a>
             </Grid>
-            <Grid item xs={3}>
-              <Link to="/">
+            <Grid item xs={2}>
+              <a target="_blank" href="https://twitter.com/a2helps">
                 <img className={classes.img} src="twitter-icon.svg" alt="Twitter Icon" />
-              </Link>
+              </a>
+            </Grid>
+            <Grid item xs={2}>
+              <a target="_blank" href="https://www.facebook.com/a2helps">
+                <img className={classes.img} src="facebook-icon.svg" alt="Facebook Icon" />
+              </a>
+            </Grid>
+            <Grid item xs={2}>
+              <a target="_blank" href="https://www.linkedin.com/company/a2-helps/">
+                <img className={classes.img} href="https://www.linkedin.com/company/a2-helps/" src="linkedin-icon.svg" alt="Linkedin Icon" />
+              </a>
             </Grid>
           </Grid>
 
