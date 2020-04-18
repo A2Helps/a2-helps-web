dd8a412f73657f2928212bf5bb831d342954ef9a
Jim Hall <jim.hall.dev@gmail.com>
2020-04-18 17:08:19 -0400

    turn on the donor page

--- src/util/feature-flags.js
+++ src/util/feature-flags.js
@@ -4,4 +4,4 @@ export const devMode = () => (process.env.NODE_ENV === 'development');
 export const prodMode = () => (process.env.NODE_ENV === 'production');
 
 // Whether to show components/pages related to Donors
-export const donorFlag = () => devMode();
+export const donorFlag = () => true;
